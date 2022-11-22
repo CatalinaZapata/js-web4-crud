17f059af475f4cc89f84fadba10b0a8b279e2423
Cata <catalina.zapata@usach.cl>
2022-11-22 15:38:21 -0300

    V0R3:Crear y borrar clientes

--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,24 @@
 const listaClientes = () => fetch('http://localhost:3000/perfil').then((respuesta) => respuesta.json()); 
 //abre una conexion a la url, le recibe en el arrow function y lo pasa JSON
 
+const crearCliente = (nombre, email) => {
+    return fetch('http://localhost:3000/perfil', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({nombre, email, id: uuid.v4()})
+    })
+}
+
+const eliminarCliente = (id) => {
+    return fetch (`http://localhost:3000/perfil/${id}`, {
+        method: 'DELETE',
+    });
+}
+
 export const clientServices = {
     listaClientes,
+    crearCliente,
+    eliminarCliente,
 }
